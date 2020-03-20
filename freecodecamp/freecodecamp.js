0fb2a2499c868e9c489963289bc2a1afd8fb910d
marcelosperalta@gmail.com <marcelosperalta@gmail.com>
2020-03-20 16:13:52 +0100

    freecodecamp

--- freecodecamp/freecodecamp.js
+++ freecodecamp/freecodecamp.js
@@ -786,13 +786,34 @@ console.log(ourArray);         // expected output: ["Happy", "J", "cat"]
 console.log(``);
 
 
-    // 
+    // Exercise: "Shopping List"
     
 /**
+ * Create a shopping list in the variable myList.
+ * The list should be a multi-dimensional array containing several sub-arrays.
  * 
+ * The first element in each sub-array should contain a string with the name of the item.
+ * The second element should be a number representing the quantity i.e.
+ * 
+ * ["Chocolate Bar", 15]
+ * 
+ * There should be at least 5 sub-arrays in the list.
  */
 
-
+var myList = [
+                ["Chocolate Bar", 15],
+                ["Egg", 10],
+                ["Milk", 1],
+                ["Bread", 1], 
+                ["Coffee", 1]
+             ];
+console.log(myList);         // expected output: [
+                             //                     [ 'Chocolate Bar', 15 ],
+                             //                     [ 'Egg', 10 ],
+                             //                     [ 'Milk', 1 ],
+                             //                     [ 'Bread', 1 ],
+                             //                     [ 'Coffee', 1 ]
+                             //                  ]
 
 console.log(``);
 
