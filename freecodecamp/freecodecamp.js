0767d0aad73bae784384ace2451a03ae873e1335
marcelosperalta@gmail.com <marcelosperalta@gmail.com>
2020-03-27 15:02:13 +0100

    freecodecamp

--- freecodecamp/freecodecamp.js
+++ freecodecamp/freecodecamp.js
@@ -852,13 +852,38 @@ functionWithArgs(2, 2);
 console.log(``);
 
 
-    // 
+    // Global Scope and Functions
     
 /**
+ * In JavaScript, scope refers to the visibility of variables.
+ * Variables which are defined outside of a function block have Global scope.
+ * This means, they can be seen everywhere in your JavaScript code.
  * 
+ * Variables which are used without the var keyword
+ * are automatically created in the global scope.
+ * This can create unintended consequences elsewhere in your code
+ * or when running a function again.
+ * You should always declare your variables with var.
  */
 
+var myGlobal = 10;
 
+function fun1() {
+  oopsGlobal = 5;
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+console.log(fun2()); // expected output: myGlobal: 10
+                     //                  undefined
 
 console.log(``);
 
