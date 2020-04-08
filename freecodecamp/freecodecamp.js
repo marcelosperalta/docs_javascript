624b48ce6dab7ed874756b79fcb956e776d3b8d7
marcelosperalta@gmail.com <marcelosperalta@gmail.com>
2020-04-08 14:48:00 +0200

    freecodecamp

--- freecodecamp/freecodecamp.js
+++ freecodecamp/freecodecamp.js
@@ -987,13 +987,20 @@ console.log(processed); // expected output: 2
 console.log(``);
 
 
-    // 
+    // Stand in Line
     
 /**
- * 
+ * In Computer Science a queue is an abstract Data Structure where items are kept in order. 
+ * New items can be added at the back of the queue 
+ * and old items are taken off from the front of the queue.
  */
 
-
+function nextInLine(arr, item) {
+    item = arr.push(item);
+    item = arr.shift();
+    return item;
+}
+console.log(nextInLine([1,2],3)); // expected output: 1
 
 console.log(``);
 
