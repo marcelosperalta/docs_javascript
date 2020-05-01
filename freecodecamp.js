67ff97800124b4134fcee1c4d14f73b069b9b202
marcelosperalta <marcelosperalta@gmail.com>
2020-05-01 18:39:44 +0200

    freecodecamp

--- freecodecamp.js
+++ freecodecamp.js
@@ -1497,13 +1497,59 @@ console.log(testElseIf(7));  // expected output: "Between 5 and 10"
 console.log(``);
 
 
-    // 
+    // Logical Order in If Else Statements
     
 /**
+ * Order is important in if, else if statements.
  * 
+ * The function is executed from top to bottom
+ * so you will want to be careful of what statement comes first.
+ * 
+ * Take these two functions as an example.
+ * 
+ * Here's the first:
+ * 
+ * function foo(x) {
+ *   if (x < 1) {
+ *     return "Less than one";
+ *   } else if (x < 2) {
+ *     return "Less than two";
+ *   } else {
+ *     return "Greater than or equal to two";
+ *   }
+ * }
+ * 
+ * And the second just switches the order of the statements:
+ * 
+ * function bar(x) {
+ *  if (x < 2) {
+ *    return "Less than two";
+ *  } else if (x < 1) {
+ *    return "Less than one";
+ *  } else {
+ *    return "Greater than or equal to two";
+ *  }
+ * }
+ * 
+ * While these two functions look nearly identical
+ *  if we pass a number to both we get different outputs.
+ * 
+ * foo(0) // "Less than one"
+ * bar(0) // "Less than two"
  */
 
-
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+console.log(orderMyLogic(7));  // expected output: "Less than 10"
+console.log(orderMyLogic(4));  // expected output: "Less than 5"
+console.log(orderMyLogic(10)); // expected output: "Greater than or equal to 10"
 
 console.log(``);
 
