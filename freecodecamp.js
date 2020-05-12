af87c3c9f8cc3df3fb6f7209c16d5f6a3ff1068f
marcelosperalta <marcelosperalta@gmail.com>
2020-05-12 23:30:26 +0200

    freecodecamp

--- freecodecamp.js
+++ freecodecamp.js
@@ -1833,11 +1833,35 @@ console.log(isLess(15, 10)); // expected output: "false"
 
 console.log(``);
 
-//
+// Return Early Pattern for Functions
 
 /**
+ * When a return statement is reached, the execution of the current function stops
+ *  and control returns to the calling location.
  *
+ * Example
+ *
+ * function myFun() {
+ *   console.log("Hello");
+ *   return "World";
+ *   console.log("byebye")
+ * }
+ * myFun();
+ *
+ * The above outputs "Hello" to the console, returns "World", but "byebye" is never output,
+ * because the function exits at the return statement.
  */
+
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return;
+  }
+  // Only change code above this line
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(abTest(2, 2)); // expected output: "8"
+console.log(abTest(-1, 2)); // expected output: "undefined"
 
 console.log(``);
 
