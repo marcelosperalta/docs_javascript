760d8119f5eb222e1c29bf19069a7302cc3868cd
marcelosperalta <marcelosperalta@gmail.com>
2020-07-31 12:05:17 +0200

    freecodecamp

--- edabit.js
+++ edabit.js
@@ -6,8 +6,35 @@
 
 // Very Easy
 
+console.log(`
+Challenges 
+JavaScript 
+Very Easy
+`);
+
+
 // How Edabit Works
 function hello() {
 	return "hello edabit.com"
 }
 console.log(hello()); // expected output: hello edabit.com
+
+
+console.log(``);
+
+
+// Return the Sum of Two Numbers
+function addition(a, b) {
+	return a + b;
+}
+console.log(addition(2, 2)); // expected output: 4
+
+
+console.log(``);
+
+// Return the Next Number from the Integer Passed
+function addition2(num) {
+	return num + 1;
+}
+console.log(addition2(1)); // expected output: 2
+console.log(addition2(-1)); // expected output: 0
