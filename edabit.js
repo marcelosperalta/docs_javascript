7e5f3c926d3495b99e291719a264b7925f293e82
marcelosperalta <marcelosperalta@gmail.com>
2020-07-31 11:57:27 +0200

    freecodecamp

---
+++ edabit.js
@@ -0,0 +1,13 @@
+// Edabit
+// https://edabit.com/
+
+// Challenges
+// JavaScript
+
+// Very Easy
+
+// How Edabit Works
+function hello() {
+	return "hello edabit.com"
+}
+console.log(hello()); // expected output: hello edabit.com
