bde94e2f49eddab3578386af762d57964d38ea06
marcelosperalta <marcelosperalta@gmail.com>
2020-07-31 12:39:25 +0200

    freecodecamp

--- edabit.js
+++ edabit.js
@@ -12,7 +12,6 @@ JavaScript
 Very Easy
 `);
 
-
 // How Edabit Works
 function hello() {
 	return "hello edabit.com"
@@ -32,9 +31,43 @@ console.log(addition(2, 2)); // expected output: 4
 
 console.log(``);
 
+
 // Return the Next Number from the Integer Passed
 function addition2(num) {
 	return num + 1;
 }
-console.log(addition2(1)); // expected output: 2
+console.log(addition2(1));  // expected output: 2
 console.log(addition2(-1)); // expected output: 0
+
+
+console.log(``);
+
+
+// Using the "&&" Operator
+function and(a, b) {
+	if (a === true && b === true){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(and(true, true));  // expected output: true
+console.log(and(false, true)); // expected output: false
+
+
+console.log(``);
+
+
+// Correct the Mistakes
+function squared(b) {
+	return b * b;
+}
+console.log(squared(5));   // expected output: 25
+console.log(squared(9));   // expected output: 81
+console.log(squared(100)); // expected output: 10000
+
+
+console.log(``);
+
+
+// 
