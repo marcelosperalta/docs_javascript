1d9f61cca281c1222cc66600ce8e832146d3e3e4
marcelosperalta <marcelosperalta@gmail.com>
2020-08-05 14:57:56 +0200

    javascript

--- freecodecamp.js
+++ freecodecamp.js
@@ -3120,13 +3120,13 @@ console.log(``);
  * It's great that we can generate random decimal numbers, 
  * but it's even more useful if we use it to generate random whole numbers.
  * 
- * Use Math.random() to generate a random decimal.
- * Multiply that random decimal by 20.
- * Use another function, 
+ * 1. Use Math.random() to generate a random decimal.
+ * 2. Multiply that random decimal by 20.
+ * 3. Use another function, 
  * Math.floor() to round the number down to its nearest whole number.
+ * 
  * Remember that Math.random() can never quite return a 1 and, 
- * because we're rounding down, 
- * it's impossible to actually get 20. 
+ * because we're rounding down, it's impossible to actually get 20.
  * This technique will give us a whole number between 0 and 19.
  * 
  * Putting everything together, this is what our code looks like:
@@ -3137,6 +3137,11 @@ console.log(``);
  * then passing the value to Math.floor() function 
  * to round the value down to the nearest whole number.
  */
+
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+console.log(randomWholeNum());
 
 console.log(``);
 
