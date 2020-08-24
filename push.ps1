fa1995b53aa6e0b24a94724a03e1060b24ba8403
marcelosperalta <marcelosperalta@gmail.com>
2020-08-24 10:27:15 +0200

    javascript

---
+++ push.ps1
@@ -0,0 +1,5 @@
+#to execute on prompt/terminal: ./push.ps1
+git pull
+git add .
+git commit -m "javascript"
+git push
---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Updating "JavaScript" Repo"
+
+git pull
+
+git add .
+
+git commit -m "javascript"
+
+git push
