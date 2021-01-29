736ec8ec7faf24df7c3db5800c4e65b80ae8238f
marcelosperalta <marcelosperalta@gmail.com>
2021-01-29 17:13:05 +0100

    javascript

---
+++ edabit_medium.js
@@ -0,0 +1,30 @@
+// How to run: *********** //
+// npm install -g nodemon  //
+// nodemon edabit.js       //
+// *********************** //
+
+// Edabit
+// https://edabit.com/
+
+// Challenges
+// JavaScript
+
+// Medium:
+
+console.log(`
+Challenges 
+JavaScript 
+Medium
+`);
+
+// Finding Nemo
+// https://edabit.com/challenge/c23dFfNiKbnguSQtq
+
+function findNemo(str) {
+    
+}
+console.log(findNemo("I am finding Nemo !")); // expected output: "I found Nemo at 4!"
+console.log(findNemo("Nemo is me"));          // expected output: "I found Nemo at 1!"
+console.log(findNemo("I Nemo am"));           // expected output: "I found Nemo at 2!"
+
+console.log(`--------------------------------------------`);
