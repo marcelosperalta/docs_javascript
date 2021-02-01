e25175ea8a3b9f049fb95a22894da03a1a63f90d
marcelosperalta <marcelosperalta@gmail.com>
2021-02-01 18:37:54 +0100

    javascript

--- edabit_very_hard.js
+++ edabit_very_hard.js
@@ -47,11 +47,25 @@ Very Hard
 // https://dev.to/frugencefidel/10-javascript-string-methods-you-should-know-4l76
 
 function correctTitle(str) {
-    
+    let array = str.toLowerCase().split(" ");
+    let newArray = [];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        newArray = newArray + " " + element[0].toUpperCase() + element.slice(1, element.length);
+    }
+    return newArray
+    .replace(/And/g, "and")
+    .replace(/The/g, "the")
+    .replace(/Of/g, "of")
+    .replace(/In/g, "in")
+    .toString()
+    .trim();
 }
-console.log(correctTitle("jOn SnoW, kINg IN thE noRth."));         // expected output: "Jon Snow, King in the North."
-console.log(correctTitle("sansa stark, lady of winterfell."));     // expected output: "Sansa Stark, Lady of Winterfell."
-console.log(correctTitle("TYRION LANNISTER, HAND OF THE QUEEN.")); // expected output: "Tyrion Lannister, Hand of the Queen."
+console.log(correctTitle("jOn SnoW, kINg IN thE noRth."));                     // expected output: "Jon Snow, King in the North."
+console.log(correctTitle("sansa stark, lady of winterfell."));                 // expected output: "Sansa Stark, Lady of Winterfell."
+console.log(correctTitle("TYRION LANNISTER, HAND OF THE QUEEN."));             // expected output: "Tyrion Lannister, Hand of the Queen."
+console.log(correctTitle("doran martell, prince of dorne, lord of sunspear.")) // expected output: "Doran Martell, Prince of Dorne, Lord of Sunspear."
+console.log(correctTitle("MANCE RAYDER, KING-BEYOND-THE-WALL."))               // expected output: "Mance Rayder, King-Beyond-the-Wall."
 
 console.log(`--------------------------------------------`);
 
