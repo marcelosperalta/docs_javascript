901580a44c4f8cc2088d7ef5304dc7b343c40a86
marcelosperalta <marcelosperalta@gmail.com>
2021-02-01 13:30:03 +0100

    javascript

--- edabit_very_hard.js
+++ edabit_very_hard.js
@@ -18,10 +18,40 @@ Very Hard
 `);
 
 // Game of Thrones: Character Titles
+// https://edabit.com/challenge/QudLBG2RvfePRMzDn
+
+// Write a function that takes a string 
+// and returns a string with the correct case 
+// for character titles in the Game of Thrones series.
 // 
-console.log(reverseOdd("Bananas"));                                        // expected output: "sananaB"
-console.log(reverseOdd("One two three four"));                             // expected output: "enO owt eerht four"
-console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length")); // expected output: "Make sure you only reverse words of odd length"
+// The words "and", "the", "of" and "in" should be lowercase.
+// All other words should have the first character as uppercase and the rest lowercase.
+// 
+// Examples:
+// 
+// correctTitle("jOn SnoW, kINg IN thE noRth.")
+// ➞ "Jon Snow, King in the North."
+// 
+// correctTitle("sansa stark, lady of winterfell.")
+// ➞ "Sansa Stark, Lady of Winterfell."
+// 
+// correctTitle("TYRION LANNISTER, HAND OF THE QUEEN.")
+// ➞ "Tyrion Lannister, Hand of the Queen."
+// 
+// Notes:
+// Punctuation and spaces must remain in their original positions.
+// Hyphenated words are considered separate words.
+// Be careful with words that contain "and", "the", "of" or "in".
+// 
+// Resource:
+// https://dev.to/frugencefidel/10-javascript-string-methods-you-should-know-4l76
+
+function correctTitle(str) {
+    
+}
+console.log(correctTitle("jOn SnoW, kINg IN thE noRth."));         // expected output: "Jon Snow, King in the North."
+console.log(correctTitle("sansa stark, lady of winterfell."));     // expected output: "Sansa Stark, Lady of Winterfell."
+console.log(correctTitle("TYRION LANNISTER, HAND OF THE QUEEN.")); // expected output: "Tyrion Lannister, Hand of the Queen."
 
 console.log(`--------------------------------------------`);
 
