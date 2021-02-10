4d36e1bbeae2ea9fd7e8edded4fdc6cfadd6342a
marcelosperalta <marcelosperalta@gmail.com>
2021-02-10 14:30:38 +0100

    javascript

--- mdn.js
+++ mdn.js
@@ -206,8 +206,17 @@ console.log(arrayMatchAll[1]); // expected output: Array ["test2", "e", "st2", "
 console.log(`-----------------------------------------------------------------------------------------------`);
 
 // String.prototype.normalize() ---------------------------------------------------------------------------- //
-// 
-
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+const name1 = '\u0041\u006d\u00e9\u006c\u0069\u0065';
+const name2 = '\u0041\u006d\u0065\u0301\u006c\u0069\u0065';
+console.log(`${name1}, ${name2}`);                // expected output: "Amélie, Amélie"
+console.log(name1 === name2);                     // expected output: false
+console.log(name1.length === name2.length);       // expected output: false
+const name1NFC = name1.normalize('NFC');
+const name2NFC = name2.normalize('NFC');
+console.log(`${name1NFC}, ${name2NFC}`);          // expected output: "Amélie, Amélie"
+console.log(name1NFC === name2NFC);               // expected output: true
+console.log(name1NFC.length === name2NFC.length); // expected output: true
 // --------------------------------------------------------------------------------------------------------- //
 
 console.log(`-----------------------------------------------------------------------------------------------`);
