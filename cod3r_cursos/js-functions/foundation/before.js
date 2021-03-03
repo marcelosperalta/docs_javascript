f1a27aee867b72eed7733ac7d6ca77b376df8fa4
marcelosperalta <marcelosperalta@gmail.com>
2021-03-03 17:46:32 +0100

    javascript

---
+++ cod3r_cursos/js-functions/foundation/before.js
@@ -0,0 +1,12 @@
+// ❗ Prerequisites
+// -> Basic knowledge of JS
+// --> Data types: number, string, boolean, array, and object
+// --> Operators: +, -, *, /, &&, ||, >=, <=, ...
+// --> If, For, While
+
+
+// ❗ Visual Studio Code
+// -> Code Runer
+// --> Ctrl + Alt + n = to run the code
+// --> Ctrl + Alt + m = stop to running the code
+// --> Manage > Extension Settings > Code-runner: Clear Previous Outpot > Check ✔️
