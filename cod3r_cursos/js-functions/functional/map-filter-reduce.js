866d20a61b37e99b79775a4147e9a246f2bd1423
marcelosperalta <marcelosperalta@gmail.com>
2021-03-08 20:26:21 +0100

    javascript

--- cod3r_cursos/js-functions/functional/map-filter-reduce.js
+++ cod3r_cursos/js-functions/functional/map-filter-reduce.js
@@ -4,3 +4,20 @@ const students = [
     { name: "Emma", score: 9.4 },
     { name: "Peter", score: 9.1 },
 ]
+
+const greatStudent = student => student.score >= 9
+const getScore = el => el.score
+const average = (acc, el, i, array) => {
+    if(i === array.length - 1) {
+        return (acc + el) / array.length
+    } else {
+        return acc + el
+    }
+}
+
+console.log(
+    students
+        .filter(greatStudent)
+        .map(getScore)
+        .reduce(average)
+) // 9.25
