4849637adc7b21e1c23f3f7637638416b7074a61
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-09-25 20:34:37 +0200

    javascript

--- course_freecodecamp/bootcamp_js_data_structure/README.md
+++ course_freecodecamp/bootcamp_js_data_structure/README.md
@@ -148,11 +148,65 @@ Now it logs ``deeper still``.
 
 ## Add Key-Value Pairs to JavaScript Objects
 
+At their most basic, _objects are just collections_ of **key-value pairs**.  
 
+In other words, they are pieces of data (values) mapped to unique identifiers called properties (keys).  
+
+Take a look at an example:  
+
+```
+const tekkenCharacter = {
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true
+};
+```
+
+The above code defines a Tekken video game character object called ``tekkenCharacter``. It has three properties, each of which map to a specific value. If you want to add an additional property, such as "origin", it can be done by assigning ``origin`` to the object:  
+
+```
+tekkenCharacter.origin = 'South Korea';
+```
+
+This uses dot notation. If you were to observe the ``tekkenCharacter`` object, it will now include the ``origin`` property. Hwoarang also had distinct orange hair. You can add this property with bracket notation by doing:  
+
+```
+tekkenCharacter['hair color'] = 'dyed orange';
+```
+
+Bracket notation is required if your property has a space in it or if you want to use a variable to name the property. In the above case, the property is enclosed in quotes to denote it as a string and will be added exactly as shown. Without quotes, it will be evaluated as a variable and the name of the property will be whatever value the variable is. Here's an example with a variable:  
+
+```
+const eyes = 'eye color';
+
+tekkenCharacter[eyes] = 'brown';
+```
 
 ## Modify an Object Nested Within an Object
 
+Now let's take a look at a slightly more complex object. Object properties can be nested to an arbitrary depth, and their values can be any type of data supported by JavaScript, including arrays and even other objects. Consider the following:  
 
+```
+let nestedObject = {
+  id: 28802695164,
+  date: 'December 31, 2016',
+  data: {
+    totalUsers: 99,
+    online: 80,
+    onlineStatus: {
+      active: 67,
+      away: 13,
+      busy: 8
+    }
+  }
+};
+```
+
+``nestedObject`` has three properties: ``id`` (value is a number), ``date`` (value is a string), and ``data`` (value is an object with its nested structure). While structures can quickly become complex, we can still use the same notations to access the information we need. To assign the value ``10`` to the ``busy`` property of the nested ``onlineStatus`` object, we use dot notation to reference the property:  
+
+```
+nestedObject.data.onlineStatus.busy = 10;
+```
 
 ## Access Property Names with Bracket Notation
 
--- course_freecodecamp/bootcamp_js_data_structure/indes.js
+++ course_freecodecamp/bootcamp_js_data_structure/indes.js
@@ -270,12 +270,96 @@ console.log(myNestedArray[0][1][2][2][1]); // deepest
 
 // Add Key-Value Pairs to JavaScript Objects
 
+const tekkenCharacter = {
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true
+};
+console.log(tekkenCharacter); // { player: 'Hwoarang', fightingStyle: 'Tae Kwon Doe', human: true }
+tekkenCharacter.origin = 'South Korea';
+console.log(tekkenCharacter); // {
+                              //   player: 'Hwoarang',
+                              //   fightingStyle: 'Tae Kwon Doe',
+                              //   human: true,
+                              //   origin: 'South Korea'
+                              // }
+tekkenCharacter['hair color'] = 'dyed orange';
+console.log(tekkenCharacter); // {
+                              //   player: 'Hwoarang',
+                              //   fightingStyle: 'Tae Kwon Doe',
+                              //   human: true,
+                              //   origin: 'South Korea',
+                              //   'hair color': 'dyed orange'
+                              // }
+const eyes = 'eye color';
+tekkenCharacter[eyes] = 'brown';
+console.log(tekkenCharacter); // {
+                              //   player: 'Hwoarang',
+                              //   fightingStyle: 'Tae Kwon Doe',
+                              //   human: true,
+                              //   origin: 'South Korea',
+                              //   'hair color': 'dyed orange',
+                              //   'eye color': 'brown'
+                              // }
 
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+console.log(foods); // {
+                    //   apples: 25,
+                    //   oranges: 32,
+                    //   plums: 28,
+                    //   bananas: 13,
+                    //   grapes: 35,
+                    //   strawberries: 27
+                    // }
 
 
 // Modify an Object Nested Within an Object
 
+let nestedObject = {
+  id: 28802695164,
+  date: 'December 31, 2016',
+  data: {
+    totalUsers: 99,
+    online: 80,
+    onlineStatus: {
+      active: 67,
+      away: 13,
+      busy: 8
+    }
+  }
+};
+nestedObject.data.onlineStatus.busy = 10;
+console.log(nestedObject); // {
+                           //   id: 28802695164,
+                           //   date: 'December 31, 2016',
+                           //   data: {
+                           //     totalUsers: 99,
+                           //     online: 80,
+                           //     onlineStatus: { active: 67, away: 13, busy: 10 }
+                           //   }
+                           // }
 
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+userActivity.data.online = 45;
+console.log(userActivity); // {
+                           //   id: 23894201352,
+                           //   date: 'January 1, 2017',
+                           //   data: { totalUsers: 51, online: 45 }
+                           // }
 
 
 // Access Property Names with Bracket Notation
