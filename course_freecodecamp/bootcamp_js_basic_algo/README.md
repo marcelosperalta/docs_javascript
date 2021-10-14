515cf43665be03364dc4f7269264a0dbb02b950b
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-10-14 18:48:49 +0200

    javascript

--- course_freecodecamp/bootcamp_js_basic_algo/README.md
+++ course_freecodecamp/bootcamp_js_basic_algo/README.md
@@ -95,3 +95,11 @@ Remove all falsy values from an array.
 Falsy values in JavaScript are ``false``, ``null``, ``0``, ``""``, ``undefined``, and ``NaN``.  
 
 Hint: Try converting each value to a Boolean.  
+
+## Where do I Belong
+
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.  
+
+For example, ``getIndexToIns([1,2,3,4], 1.5)`` should return ``1`` because it is greater than ``1`` (index 0), but less than ``2`` (index 1).  
+
+Likewise, ``getIndexToIns([20,3,5], 19)`` should return ``2`` because once the array has been sorted it will look like ``[3,5,20]`` and ``19`` is less than ``20`` (index 2) and greater than ``5`` (index 1).  
--- course_freecodecamp/bootcamp_js_basic_algo/index.js
+++ course_freecodecamp/bootcamp_js_basic_algo/index.js
@@ -414,3 +414,106 @@ console.log(bouncer([7, "ate", "", false, 9]));             // [7, "ate", 9]
 console.log(bouncer(["a", "b", "c"]));                      // ["a", "b", "c"]
 console.log(bouncer([false, null, 0, NaN, undefined, ""])); // []
 console.log(bouncer([null, NaN, 1, 2, undefined]));         // [1, 2]
+
+
+// Where do I Belong
+
+// let array = [];
+// console.log(array.length); // 0
+
+function getIndexToIns(arr, num) {
+
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  // compareFunction
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#description
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  let arraySorted = arr.sort(compareNumbers)
+
+  for (let i = 0; i < arraySorted.length; i++) {
+    if (num <= arraySorted[i]) {
+      return arraySorted.indexOf(arraySorted[i]);
+    }
+  }
+
+  return arraySorted.length;
+}
+console.log(getIndexToIns([2, 5, 10], 15));           // 3
+console.log(getIndexToIns([], 1));                    // 0
+console.log(getIndexToIns([5, 3, 20, 3], 5));         // 2
+console.log(getIndexToIns([40, 60], 50));             // 1
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30)); // 2
+console.log(getIndexToIns([3, 10, 5], 3));            // 0
+
+
+// or
+
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num)
+      return i;
+  }
+
+  return arr.length;
+}
+console.log(getIndexToIns([2, 5, 10], 15));           // 3
+console.log(getIndexToIns([], 1));                    // 0
+console.log(getIndexToIns([5, 3, 20, 3], 5));         // 2
+console.log(getIndexToIns([40, 60], 50));             // 1
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30)); // 2
+console.log(getIndexToIns([3, 10, 5], 3));            // 0
+
+// or
+
+function getIndexToIns(arr, num) {
+  return arr.filter(val => num > val).length;
+}
+console.log(getIndexToIns([2, 5, 10], 15));           // 3
+console.log(getIndexToIns([], 1));                    // 0
+console.log(getIndexToIns([5, 3, 20, 3], 5));         // 2
+console.log(getIndexToIns([40, 60], 50));             // 1
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30)); // 2
+console.log(getIndexToIns([3, 10, 5], 3));            // 0
+
+// or 
+
+function getIndexToIns(arr, num) {
+  // sort and find right index
+  let index = arr
+    .sort((curr, next) => curr - next)
+    .findIndex(currNum => num <= currNum);
+  // Returns index or total length of arr
+  return index === -1 ? arr.length : index;
+}
+console.log(getIndexToIns([2, 5, 10], 15));           // 3
+console.log(getIndexToIns([], 1));                    // 0
+console.log(getIndexToIns([5, 3, 20, 3], 5));         // 2
+console.log(getIndexToIns([40, 60], 50));             // 1
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30)); // 2
+console.log(getIndexToIns([3, 10, 5], 3));            // 0
+
+// or
+
+function getIndexToIns(arr, num) {
+  return arr
+    .concat(num)
+    .sort((a, b) => a - b)
+    .indexOf(num);
+}
+console.log(getIndexToIns([2, 5, 10], 15));           // 3
+console.log(getIndexToIns([], 1));                    // 0
+console.log(getIndexToIns([5, 3, 20, 3], 5));         // 2
+console.log(getIndexToIns([40, 60], 50));             // 1
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30)); // 2
+console.log(getIndexToIns([3, 10, 5], 3));            // 0
