f1ed8345be048559fe38462e5cf7a33390f8970d
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-10-15 15:26:32 +0200

    javascript

--- course_freecodecamp/bootcamp_js_basic_algo/index.js
+++ course_freecodecamp/bootcamp_js_basic_algo/index.js
@@ -534,3 +534,37 @@ console.log(mutation(["ate", "date"]));    // false
 console.log(mutation(["hello", "hey"]));   // false
 console.log(mutation(["hello", "Hello"])); // true
 console.log(mutation(["Noel", "Ole"]));    // true
+
+// or 
+
+function mutation(arr) {
+  return arr[1]
+    .toLowerCase()
+    .split("")
+    .every(function(letter) {
+      return arr[0].toLowerCase().indexOf(letter) != -1;
+    });
+}
+console.log(mutation(["floor", "for"]));   // true
+console.log(mutation(["ate", "date"]));    // false
+console.log(mutation(["hello", "hey"]));   // false
+console.log(mutation(["hello", "Hello"])); // true
+console.log(mutation(["Noel", "Ole"]));    // true
+
+// or
+
+// (Recursive)
+function mutation([ target, test ], i = 0) {
+  target = target.toLowerCase();
+  test = test.toLowerCase();
+  return i >= test.length
+    ? true
+    : !target.includes(test[i])
+      ? false
+      : mutation([ target, test ], i + 1);
+}
+console.log(mutation(["floor", "for"]));   // true
+console.log(mutation(["ate", "date"]));    // false
+console.log(mutation(["hello", "hey"]));   // false
+console.log(mutation(["hello", "Hello"])); // true
+console.log(mutation(["Noel", "Ole"]));    // true
