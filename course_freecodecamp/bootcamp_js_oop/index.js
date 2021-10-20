d8270bc89c3d6aa953a0d314dc8e0b6a72844b97
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-10-20 13:30:45 +0200

    javascript

--- course_freecodecamp/bootcamp_js_oop/index.js
+++ course_freecodecamp/bootcamp_js_oop/index.js
@@ -14,3 +14,48 @@ let duck = {
         numLegs: 4
     };
     console.log(dog); // { name: 'Chip', numLegs: 4 }
+
+
+// Use Dot Notation to Access the Properties of an Object
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+console.log(duck.name);
+
+    // // Test
+    // Print both properties of the dog object to your console.
+    let dog = {
+      name: "Spot",
+      numLegs: 4
+    };
+    console.log(dog.name, dog.numLegs); // Spot 4
+
+
+// Create a Method on an Object
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+  sayName: function() {return "The name of this duck is " + duck.name + ".";}
+};
+duck.sayName();
+
+    // // Test
+    // Using the dog object, give it a method called sayLegs. 
+    // The method should return the sentence This dog has 4 legs.
+    let dog = {
+      name: "Spot",
+      numLegs: 4,
+      sayLegs: function() {return "This dog has " + dog.numLegs + " legs."}
+    };
+    console.log(dog.sayLegs());
+
+    //or
+    let dog = {
+      name: "Spot",
+      numLegs: 4,
+      sayLegs: function() {return `This dog has ${dog.numLegs} legs.`}
+    };
+    console.log(dog.sayLegs());
