5461e71a2841205b4d7f3a23dfd6eb6f3b9d7e6a
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-11-08 15:48:19 +0100

    javascript

--- course_freecodecamp/bootcamp_js_oop/index.js
+++ course_freecodecamp/bootcamp_js_oop/index.js
@@ -647,4 +647,17 @@ Dog.prototype = {
       };
     };
 
-    
+    let funModule = (function(){
+      return {
+         isCuteMixin: function(obj) {
+            obj.isCute = function() {
+              return true;
+            };
+          },
+          singMixin: function(obj) {
+            obj.sing = function() {
+              console.log("Singing to an awesome tune");
+            };
+          }
+      }
+    })();
