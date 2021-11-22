c9b4b2ff00ced3fa6539e221dc120b700d0ac942
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-11-22 13:36:43 +0100

    javascript

--- course_freecodecamp/bootcamp_js_func_program/README.md
+++ course_freecodecamp/bootcamp_js_func_program/README.md
@@ -27,3 +27,24 @@ Functions that can be assigned to a variable, passed into another function, or r
 The functions that take a function as an argument, or return a function as a return value are called **higher order** functions.  
 
 When functions are passed in to or returned from another function, then those functions which were passed in or returned can be called a **lambda**.  
+
+## Understand the Hazards of Using Imperative Code
+
+In English (and many other languages), the imperative tense is used to give commands. Similarly, an **imperative style in programming** is one that gives the computer a set of statements to perform a task.  
+
+In contrast, **functional programming** is a form of declarative programming. You tell the computer what you want done by calling a method or function.  
+
+Consider the scenario: you are browsing the web in your browser, and want to track the tabs you have opened. Let's try to model this using some simple object-oriented code.  
+
+A Window object is made up of tabs, and you usually have more than one Window open. The titles of each open site in each Window object is held in an array. After working in the browser (opening new tabs, merging windows, and closing tabs), you want to print the tabs that are still open. Closed tabs are removed from the array and new tabs (for simplicity) get added to the end of it.  
+
+The code editor shows an implementation of this functionality with functions for ``tabOpen()``, ``tabClose()``, and ``join()``. The array ``tabs`` is part of the Window object that stores the name of the open pages.  
+
+## Avoid Mutations and Side Effects Using Functional Programming
+
+If you haven't already figured it out, the issue in the previous challenge was with the ``splice`` call in the ``tabClose()`` function. Unfortunately, ``splice`` changes the original array it is called on, so the second call to it used a modified array, and gave unexpected results.  
+
+**One of the core principles of functional programming is to not change things.**  
+Changes lead to bugs. It's easier to prevent bugs knowing that your functions don't change anything, including the function arguments or any global variable.  
+
+Recall that in **functional programming**, changing or altering things is called **mutation**, and the outcome is called a **side effect**. A function, ideally, should be a **pure function**, meaning that it does not cause any side effects.  
--- course_freecodecamp/bootcamp_js_func_program/index.js
+++ course_freecodecamp/bootcamp_js_func_program/index.js
@@ -63,5 +63,74 @@
 
     console.log(
     tea4GreenTeamFCC,
-    tea4BlackTeamFCC
-    );
+    tea4BlackTeamFCC);
+
+// Understand the Hazards of Using Imperative Code
+
+// Examine the code in the editor. It's using a method that has side effects in the program, 
+// causing incorrect behaviour. The final list of open tabs, stored in "finalTabs.tabs", 
+// should be "['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium', 'new tab', 'Netflix', 'YouTube', 
+// 'Vine', 'GMail', 'Work mail', 'Docs', 'freeCodeCamp', 'new tab']" but 
+// the list produced by the code is slightly different.
+
+// Change "Window.prototype.tabClose" so that it removes the correct tab.
+
+    // tabs is an array of titles of each site open within the window
+    const Window = function(tabs) {
+        this.tabs = tabs; // We keep a record of the array inside the object
+    };
+    
+    // When you join two windows into one window
+    Window.prototype.join = function(otherWindow) {
+        this.tabs = this.tabs.concat(otherWindow.tabs);
+        return this;
+    };
+    
+    // When you open a new tab at the end
+    Window.prototype.tabOpen = function(tab) {
+        this.tabs.push('new tab'); // Let's open a new tab for now
+        return this;
+    };
+    
+    // When you close a tab
+    Window.prototype.tabClose = function(index) {
+    
+        // Only change code below this line
+    
+        const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+        const tabsAfterIndex = this.tabs.splice(1);         // Get the tabs after the tab
+    
+        this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+    
+        // Only change code above this line
+    
+        return this;
+    };
+    
+    // Let's create three browser windows
+    const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+    const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+    const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+    
+    // Now perform the tab opening, closing, and other operations
+    const finalTabs = socialWindow
+        .tabOpen() // Open a new tab for cat memes
+        .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+        .join(workWindow.tabClose(1).tabOpen());
+    console.log(finalTabs.tabs);
+
+
+// Avoid Mutations and Side Effects Using Functional Programming
+
+    // Fill in the code for the function incrementer so it returns the value 
+    // of the global variable fixedValue increased by one.
+    // The global variable
+    let fixedValue = 4;
+
+    function incrementer() {
+    // Only change code below this line
+        let newValue = fixedValue + 1;
+        return newValue;
+    // Only change code above this line
+    }
+    console.log(incrementer()); // 5
