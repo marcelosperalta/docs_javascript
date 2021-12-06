0583dba5078b7a7658e0a5f802625609cd09e03d
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-12-06 13:33:07 +0100

    javascript

--- course_freecodecamp/bootcamp_js_func_program/index.js
+++ course_freecodecamp/bootcamp_js_func_program/index.js
@@ -1344,3 +1344,65 @@ console.log(usersObj);
 
 // Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
 
+    // Complete the code for the "squareList" function using any combination of 
+    // "map()", "filter()", and "reduce()". The function should return a new array 
+    // containing the squares of only the positive integers (decimal numbers are not integers) 
+    // when an array of real numbers is passed to it. An example of an array of real numbers is 
+    // "[-3, 4.8, 5, 3, -3.2]".
+
+    // Note: Your function should not use any kind of "for" or "while" loops or
+    // the "forEach()" function.
+
+    // Output:
+    // squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3]) should return [9, 100, 49].
+    // squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]) should return [16, 1764, 36].
+
+    const squareList = arr => {
+      // Only change code below this line
+      return arr;
+      // Only change code above this line
+    };
+    
+    const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+    console.log(squaredIntegers);
+
+    // solution: 
+
+    const squareList = arr => {
+        let newArr = 
+        arr
+        .filter(number => Number.isInteger(number) === true && number >= 0)
+        .map(number => number * number);
+
+        return newArr;
+    };
+
+    const squaredIntegers1 = squareList([-3, 4.8, 5, 3, -3.2]);
+    console.log(squaredIntegers1); // [ 25, 9 ]
+    
+    const squaredIntegers2 = squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3]);
+    console.log(squaredIntegers2); // [ 9, 100, 49 ]
+
+    const squaredIntegers3 = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
+    console.log(squaredIntegers3); // [ 16, 1764, 36 ]
+
+    // or 
+
+    const squareList = (arr) => {
+      return arr
+              .filter(num => num > 0 && num % parseInt(num) === 0)
+              .map(num => Math.pow(num, 2));
+    };
+    
+    const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+    console.log(squaredIntegers);
+
+    // or
+
+    const squareList = arr => {
+      return arr.reduce((sqrIntegers, num) => {
+        return Number.isInteger(num) && num > 0
+          ? sqrIntegers.concat(num * num)
+          : sqrIntegers;
+      }, []);
+    };
