0d5f6553437ea15997e8cdfd19dbecc1144bcafd
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2021-12-11 17:59:55 +0100

    javascript

---
+++ course_freecodecamp/bootcamp_js_algo_iterm/README.md
@@ -0,0 +1,10 @@
+# Intermediate Algorithm Scripting
+
+_source:_  
+**freeCodeCamp**  
+:link: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures#intermediate-algorithm-scripting  
+
+Now that you know the basics of algorithmic thinking, along with OOP and Functional Programming, test your skills with the Intermediate Algorithm Scripting challenges.  
+
+## Sum All Numbers in a Range
+
---
+++ course_freecodecamp/bootcamp_js_algo_iterm/index.js
@@ -0,0 +1,19 @@
+// Sum All Numbers in a Range
+
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus 
+// the sum of all the numbers between them. The lowest number will not always come first.
+
+// For example, "sumAll([4,1])" should return "10" because sum of 
+// all the numbers between 1 and 4 (both inclusive) is "10".
+
+// sumAll([1, 4])  should return 10.
+// sumAll([4, 1])  should return 10.
+// sumAll([5, 10]) should return 45.
+// sumAll([10, 5]) should return 45.
+
+function sumAll(arr) {
+    
+
+    return ;
+}
+sumAll([1, 4]);
