8e7d0ea3d754e06c9bfbd71f64c1aafc9a371dfa
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-01-02 10:48:35 +0100

    javascript

--- course_freecodecamp/bootcamp_js_algo_iterm/README.md
+++ course_freecodecamp/bootcamp_js_algo_iterm/README.md
@@ -25,3 +25,5 @@ Pig Latin is a way of altering English Words. The rules are as follows:
 - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ``ay`` to it.
 
 - If a word begins with a vowel, just add ``way`` at the end.
+
+## Search and Replace
--- course_freecodecamp/bootcamp_js_algo_iterm/index.js
+++ course_freecodecamp/bootcamp_js_algo_iterm/index.js
@@ -554,3 +554,52 @@ console.log(translatePigLatin("algorithm"));  // algorithmway
 console.log(translatePigLatin("eight"));      // eightway
 console.log(translatePigLatin("schwartz"));   // artzschway
 console.log(translatePigLatin("rhythm"));     // rhythmay
+
+
+// Search and Replace
+
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+// First argument is the sentence to perform the search and replace on.
+
+// Second argument is the word that you will be replacing (before).
+
+// Third argument is what you will be replacing the second argument with (after).
+
+// Note: Preserve the case of the first character in the original word when you are replacing it. 
+// For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+
+myReplace("Let us go to the store", "store", "mall")                // Let us go to the mall.
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting")     // He is Sitting on the couch.
+myReplace("I think we should look up there", "up", "Down")          // I think we should look down there.
+myReplace("This has a spellngi error", "spellngi", "spelling")      // This has a spelling error.
+myReplace("His name is Tom", "Tom", "john")                         // His name is John.
+myReplace("Let us get back to more Coding", "Coding", "algorithms") // Let us get back to more Algorithms.
+
+function myReplace(str, before, after) {
+
+  // Find index where before is on string
+  var index = str.indexOf(before);
+
+  // Check to see if the first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // Change the after word to be capitalized before we use it.
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    // Change the after word to be uncapitalized before we use it.
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+
+  // Now replace the original str with the edited one.
+  str = str.replace(before, after);
+
+  return console.log(str);
+}
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+// A quick brown fox leaped over the lazy dog
+myReplace("Let us go to the store", "store", "mall")                // Let us go to the mall.
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting")     // He is Sitting on the couch.
+myReplace("I think we should look up there", "up", "Down")          // I think we should look down there.
+myReplace("This has a spellngi error", "spellngi", "spelling")      // This has a spelling error.
+myReplace("His name is Tom", "Tom", "john")                         // His name is John.
+myReplace("Let us get back to more Coding", "Coding", "algorithms") // Let us get back to more Algorithms.
