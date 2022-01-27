d97c3e14c7aabfc88606791f738be81a5df3b3dd
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2022-01-27 16:56:51 +0100

    javascript

--- course_freecodecamp/bootcamp_js_5_projects/index.js
+++ course_freecodecamp/bootcamp_js_5_projects/index.js
@@ -15,9 +15,33 @@
 // We'll also pass strings with special symbols, such as ``2A3*3a2``, ``2A3 3a2``, 
 // and ``2_A3*3#A2``.
 
+// palindrome("eye")                            // true
+// palindrome("_eye")                           // true
+// palindrome("race car")                       // true
+// palindrome("not a palindrome")               // false
+// palindrome("A man, a plan, a canal. Panama") // true
+// palindrome("never odd or even")              // true
+// palindrome("nope")                           // false
+// palindrome("almostomla")                     // false
+// palindrome("My age is 0, 0 si ega ym.")      // true
+// palindrome("1 eye for of 1 eye.")            // false
+// palindrome("0_0 (: /-\ :) 0-0")              // true
+// palindrome("five|\_/|four")                  // false
+
 function palindrome(str) {
 
 
     return true;
 }
-palindrome("eye");
+palindrome("eye")                            // true
+palindrome("_eye")                           // true
+palindrome("race car")                       // true
+palindrome("not a palindrome")               // false
+palindrome("A man, a plan, a canal. Panama") // true
+palindrome("never odd or even")              // true
+palindrome("nope")                           // false
+palindrome("almostomla")                     // false
+palindrome("My age is 0, 0 si ega ym.")      // true
+palindrome("1 eye for of 1 eye.")            // false
+palindrome("0_0 (: /-\ :) 0-0")              // true
+palindrome("five|\_/|four")                  // false
